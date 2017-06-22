b263b05960fe7144035096761d21b0632232c98c
Rhian Chavez <rhianchavez11@fb.com>
2017-06-22 11:20:34 -0700

    a lot

--- Day2Lab/PhotosViewController.swift
+++ Day2Lab/PhotosViewController.swift
@@ -23,14 +23,14 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
-        let session = URLSession(configuration: .default,    delegate: nil, delegateQueue: OperationQueue.main)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
         let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data,
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print(dataDictionary)
+                //print(dataDictionary)
                 
                 // TODO: Get the posts and store in posts property
                 
@@ -71,18 +71,10 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
             let url = URL(string: urlString)
             cell.Picture.af_setImage(withURL: url!)
         }
-        
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        <#code#>
     }
-    */
-
 }
